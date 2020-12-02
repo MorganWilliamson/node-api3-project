0457ea2842a1b0c8df81c0f9575443f90f4dff1b
MorganWilliamson <mwilliamson7695@gmail.com>
2020-12-02 16:36:56 -0600

    validatePost() built.

--- api/middleware.js
+++ api/middleware.js
@@ -7,13 +7,15 @@ const logger = (req, res, next) => {
     console.log(req.method, req.url, Date.now());
     next();
   };
-  
-  // validateUserId(): check DB for a valid user. Return valid user or 404 otherwise. 
+ 
+// validateUserId(): check DB for a valid user. Return valid user or 404 otherwise. 
 const validateUserId = async (req, res, next) => {
     const { id } = req.params;
     const user = await Users.getById(id)
 
     if (!user) {
+        res.status(404).json({ message: "request requires a user id" })
+    } else if (!user.id) {
         res.status(400).json({ message: "invalid user id" })
     } else {
         req.user = user;
@@ -21,12 +23,34 @@ const validateUserId = async (req, res, next) => {
     }
 };
   
-  // validateUser(): validates that new users have a name, returns a 400 otherwise. 
+// validateUser(): validates that new users have a name, returns a 400 otherwise. 
+const validateUser = async (req, res, next) => {
+    if (!req) {
+        res.status(400).json({ message: "missing user data" })
+    } else if (!req.body) {
+        res.status(400).json({ message: "missing required name field" })
+    } else {
+        next();
+    }
+};
   
-  
-  // validatePost()
+// validatePost(): similar to validateUser, but with posts. 
+const validatePost = async (req, res, next) => {
+    const { id } = req.params;
+    const post = await Posts.getById(id);
+
+    if (!post) {
+        res.status(400).json({ message: "missing post data" })
+    } else if (!post.text) {
+        res.status(400).json({ message: "missing required text field" })
+    } else {
+        next();
+    }
+}
   
   module.exports = {
       logger,
       validateUserId,
+      validateUser,
+      validatePost,
   }
